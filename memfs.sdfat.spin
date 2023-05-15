avsa242/sdfat-spin

commit 4945c399f6d7547528c38172af965fe3d4465b30

fopen(): add support for creating files if they don't exist, and O_CREAT is specified sdfat-eedump.spin: simplify the file opening process - remove the fcreate() call and just call fopen(), now with O_CREAT set

memfs.sdfat.spin
@@ -5,7 +5,7 @@
     Description: FAT32-formatted SDHC/XC driver
     Copyright (c) 2023
     Started Jun 11, 2022
-    Updated May 14, 2023
+    Updated May 15, 2023
     See end of file for terms of use.
     --------------------------------------------
 }
@@ -254,7 +254,7 @@ PUB fcount_clust{}: t_clust | fat_entry, fat_sector, nxt_entry
             fat_entry := nxt_entry
             _fclust_tot := ++t_clust            ' track total # of clusters used
             'dprintf1(@"    nxt_entry = %02x\n\r", nxt_entry)
-            if ( clust_is_eoc(nxt_entry) )
+            if ( fat_entry_is_eoc(nxt_entry) )
                 'dprintf2(@"    EOC reached; total %d/%d clusters\n\r", _fclust_tot, t_clust)
                 'dstrln(@"fcount_clust() [ret]")
                 return t_clust
@@ -466,33 +466,47 @@ PUB find_last_clust{}: cl_nr | fat_ent, resp, fat_sect
             quit    ' abort EFSCORRUPT?
         'dprintf1(@"    cl_nr: %x\n\r", cl_nr)
         'dprintf1(@"    fat_ent: %x\n\r", fat_ent)
-    while not (clust_is_eoc(fat_ent))
+    while not (fat_entry_is_eoc(fat_ent))
     'dprintf1(@"    last clust is %x\n\r", cl_nr)
     _fclust_last := cl_nr
     'dstrln(@"find_last_clust(): [ret]")
     return cl_nr
 
-PUB fopen(fn_str, mode): status
+PUB fopen(fn_str, mode): status | dirent_nr
 ' Open file for subsequent operations
 '   Valid values:
 '       fn_str: pointer to string containing filename (must be space padded)
-'       mode: O_RDONLY (1), or O_WRITE (2), O_RDWR (3)
+'       mode: (bitwise OR together to combine modes)
+'           O_RDONLY (1): read-only access
+'           O_WRITE (2): overwrite access (won't grow file size)
+'           O_RDWR (3): read/write access (= O_RDONLY | O_WRITE)
+'           O_CREAT (4): create the file if it doesn't exist
+'           O_APPEND (8): write access, always append new data to the end of the file
+'               (fseek() calls are ignored)
+'           O_TRUNC (16): truncate the file to 0 bytes when opening
 '   Returns:
 '       file number (dirent #) if successful,
 '       or error
     'dstrln(@"fopen():")
-    if (fnumber{} => 0)                              ' file is already open
-        'dstrln_err(@"    error: already open")   'xxx bit of duplication with FOpenEnt()
+    if ( fnumber{} => 0 )                       ' file is already open
+        'dstrln_err(@"    error: already open") 'xxx bit of duplication with FOpenEnt()
         return EOPEN
-    status := find(fn_str)                      ' look for file by name
-'    if (_fmode & O_CREAT)
-'        status := fcreate(fn_str, FATTR_ARC)
-'    else
-    if (status == ENOTFOUND)                ' file not found
-        'dstrln_err(@"    error: not found")
-        'dprintf1(@"fopen(): [ret %d]\n\r", status)
-        return ENOTFOUND
+
+    dirent_nr := find(fn_str)                   ' look for file by name
     'dprintf1(@"    found file, dirent # %d\n\r", status)
+
+    if ( dirent_nr == ENOTFOUND )               ' file not found
+        if ( mode & O_CREAT )                   ' create it, if the option is given
+            status := fcreate(fn_str, FATTR_ARC)
+            mode &= !O_CREAT                    ' strip off the create bit for fopen_ent()
+            'dprintf1(@"    fopen() [ret %d]\n\r", status)
+            if ( status < 0 )                   ' error creating dirent
+                return
+        else
+            'dstrln_err(@"    error: not found and O_CREAT not specified")
+            'dprintf1(@"fopen(): [ret %d]\n\r", status)
+            return ENOTFOUND
+
     status := fopen_ent(status, mode)
     'dprintf1(@"fopen(): [ret %d]\n\r", status)
 
@@ -522,6 +536,7 @@ PUB fopen_ent(file_nr, mode): status
     'dhexdump(@_dirent, 0, 4, 32, 16)
     'dprintf3(@"    opened file/dirent # %d (%s.%s)\n\r", fnumber{}, @_fname, @_fext) 'xxx corruption in debug output?
     'dprintf1(@"    opened file/dirent # %d\n\r", fnumber{})
+
     { set up the initial state:
         * set the seek pointer to the file's beginning
         * cache the file's open mode

sdfat-eedump.spin
@@ -5,7 +5,7 @@
     Description: FATfs on SD: dump a 64KB EEPROM to a file on SD/FAT
     Copyright (c) 2023
     Started Aug 25, 2022
-    Updated May 14, 2023
+    Updated May 15, 2023
     See end of file for terms of use.
     --------------------------------------------
 }
@@ -46,36 +46,28 @@ DAT
 
 VAR
 
-    byte _ee_page[512]
+    byte _ee_buff[512]
 
 PUB main() | err, dirent, ee_addr, ee_subpg
 
     setup()
 
-    err := sd.fcreate(@_fname, sd#FATTR_ARC)
-    if (err < 0)
-        if ( err <> sd.EEXIST )
-            perror(@"Error creating file: ", err)
-            repeat
-    ser.strln(@"done")
-
     { open the file for writing, but truncate it to 0 first, in case it already exists and
         is non-zero in size }
-    err := sd.fopen(@_fname, sd#O_WRITE | sd#O_TRUNC | sd#O_APPEND)
+    err := sd.fopen(@_fname, sd#O_WRITE | sd#O_TRUNC | sd#O_APPEND | sd#O_CREAT)
     if (err < 0)
         perror(@"Error opening: ", err)
         repeat
 
     ser.printf1(@"Opened %s\n\r", @_fname)
-
     ser.strln(@"dumping EEPROM to file...")
     repeat ee_addr from 0 to 65024 step 512
         ser.printf1(@"EE addr: %d\n\r", ee_addr)
         { read 4 pages from the EE at a time to fill the SD buffer for better efficiency }
         repeat ee_subpg from 0 to 3
-            ee.rd_block_lsbf(@_ee_page + (ee.page_size() * ee_subpg), {
-}           ee_addr + (ee.page_size() * ee_subpg), ee.page_size())
-        sd.fwrite(@_ee_page, 512)
+            ee.rd_block_lsbf(   @_ee_buff + (ee.page_size() * ee_subpg), ...
+                                ee_addr + (ee.page_size() * ee_subpg), ee.page_size())
+        sd.fwrite(@_ee_buff, 512)
 
     ser.str(@"closing file...")
     sd.fclose()

commit e178e1aea7bdd8e061840737cdb1edffbc7eb420

fopen(): - fix nasty bug introduced in 4945c399f6d7547528c38172af965fe3d4465b30 where the wrong value for the dirent was getting passed to fopen_ent()

memfs.sdfat.spin
@@ -246,6 +246,7 @@ PUB fallocate{}: status | flc, cl_free, fat_sect
     { allocate/write EOC in the newly found free cluster }
     status := alloc_clust(cl_free)
     _fclust_last := status
+    'dprintf1(@"fallocate() [ret: %d]\n\r", status)
 
 PUB fcount_clust{}: t_clust | fat_entry, fat_sector, nxt_entry
 ' Count number of clusters used by currently open file
@@ -265,6 +266,7 @@ PUB fcount_clust{}: t_clust | fat_entry, fat_sector, nxt_entry
             nxt_entry := read_fat_entry(fat_entry)
             fat_entry := nxt_entry
             _fclust_tot := ++t_clust            ' track total # of clusters used
+            'dprintf1(@"    total clusts = %d\n\r", _fclust_tot)
             'dprintf1(@"    nxt_entry = %02x\n\r", nxt_entry)
             if ( fat_entry_is_eoc(nxt_entry) )
                 'dprintf2(@"    EOC reached; total %d/%d clusters\n\r", _fclust_tot, t_clust)
@@ -272,6 +274,7 @@ PUB fcount_clust{}: t_clust | fat_entry, fat_sector, nxt_entry
                 return t_clust
         fat_sector++
     while ( fat_sector < (fat1_start() + sects_per_fat()) )
+    'dstrln(@"fcount_clust() [ret]")
 
 PUB fcreate(fn_str, attrs): status | dirent_nr, ffc
 ' Create file
@@ -300,7 +303,7 @@ PUB fcreate(fn_str, attrs): status | dirent_nr, ffc
 
     _fmode := O_CREAT
     { set up the file's initial metadata }
-    'dstrln(@"setting up dirent")
+    'dstrln(@"    setting up dirent")
     fset_fname(fn_str)
     fset_ext(fn_str+9)   'XXX expects string at fn_str to be in '8.3' format, _with_ the period
     fset_attrs(attrs)
@@ -397,7 +400,7 @@ PUB find(ptr_str): dirent | rds, endofdir, name_tmp[3], ext_tmp, fn_tmp[4], d
         'dprintf2(@"    dirent's filename is %s.%s\n\r", fname(), fname_ext())
         if ( strcomp(ptr_str, dirent_filename(@fn_tmp)) )
             'dstrln(@"    match found")
-            'dstrln(@"find() [ret]")
+            'dprintf1(@"find() [ret: %d]\n\r", d)
             return d
     'dstrln(@"find() [ret]")
     return ENOTFOUND
@@ -484,7 +487,7 @@ PUB find_last_clust{}: cl_nr | fat_ent, resp, fat_sect
     'dstrln(@"find_last_clust(): [ret]")
     return cl_nr
 
-PUB fopen(fn_str, mode): status | dirent_nr
+PUB fopen(fn_str, mode): status
 ' Open file for subsequent operations
 '   Valid values:
 '       fn_str: pointer to string containing filename (must be space padded)
@@ -504,15 +507,15 @@ PUB fopen(fn_str, mode): status | dirent_nr
         'dstrln_err(@"    error: already open") 'xxx bit of duplication with FOpenEnt()
         return EOPEN
 
-    dirent_nr := find(fn_str)                   ' look for file by name
+    status := find(fn_str)                   ' look for file by name
     'dprintf1(@"    found file, dirent # %d\n\r", status)
 
-    if ( dirent_nr == ENOTFOUND )               ' file not found
+    if ( status == ENOTFOUND )               ' file not found
         if ( mode & O_CREAT )                   ' create it, if the option is given
             status := fcreate(fn_str, FATTR_ARC)
             mode &= !O_CREAT                    ' strip off the create bit for fopen_ent()
-            'dprintf1(@"    fopen() [ret %d]\n\r", status)
             if ( status < 0 )                   ' error creating dirent
+                'dprintf1(@"    fopen() [ret %d]\n\r", status)
                 return
         else
             'dstrln_err(@"    error: not found and O_CREAT not specified")
@@ -536,8 +539,10 @@ PUB fopen_ent(file_nr, mode): status
         'dstrln_warn(@"    already open")
         return EOPEN
 
+    'dprintf2(@"    reading rootdir sect %d+%d...", root_dir_sect{},  dirent_to_sect(file_nr))
     sd.rd_block(@_meta_buff, (root_dir_sect{} + dirent_to_sect(file_nr)))
     _meta_lastread := META_DIR
+    'dstrln(@"    read ok")
 
     read_dirent(file_nr & $0f)               ' cache dirent metadata
     if (dirent_never_used{})
@@ -548,7 +553,6 @@ PUB fopen_ent(file_nr, mode): status
             return
     _file_nr := file_nr 'xxx hacky
     'dhexdump(@_dirent, 0, 4, 32, 16)
-    'dprintf3(@"    opened file/dirent # %d (%s.%s)\n\r", fnumber{}, @_fname, @_fext) 'xxx corruption in debug output?
     'dprintf1(@"    opened file/dirent # %d\n\r", fnumber{})
 
     { set up the initial state:
@@ -569,6 +573,7 @@ PUB fopen_ent(file_nr, mode): status
     ifnot (mode & O_CREAT)                      ' don't bother checking which cluster # is
         find_last_clust{}                         '   the file's last if creating it
     'dstrln(@"fopen_ent(): [ret]")
+    'dprintf2(@"fcl_tot: %d  cl_sz: %d\n\r", _fclust_tot, clust_sz())
     return fnumber{}
 
 PUB fread(ptr_dest, nr_bytes): nr_read | nr_left, movbytes, resp
@@ -753,6 +758,8 @@ PUB fwrite(ptr_buff, len): status | sect_wrsz, nr_left, resp
     { determine file's max phys. size on disk to see if more space needs to be allocated }
     fcount_clust{}
     if ( (ftell{} + len) > fphys_size{} )       ' is req'd size larger than allocated space?
+        'dprintf1(@"    ftell() + len = %d\n\r", ftell()+len)
+        'dprintf1(@"    fphys_size()-1 = %d\n\r", fphys_size()-1)
         'dstrln(@"    current seek+req'd write len will be greater than file's allocated space")
         ifnot (_fmode & O_APPEND)   ' xxx make sure this is necessary
             'dstrln(@"    error: bad seek (not opened for appending)")

sdfat-eedump.spin
@@ -54,7 +54,7 @@ PUB main() | err, dirent, ee_addr, ee_subpg
 
     { open the file for writing, but truncate it to 0 first, in case it already exists and
         is non-zero in size }
-    err := sd.fopen(@_fname, sd#O_WRITE | sd#O_TRUNC | sd#O_APPEND | sd#O_CREAT)
+    err := sd.fopen(@_fname, sd#O_WRITE | sd#O_TRUNC | sd#O_APPEND | sd#O_CREAT )
     if (err < 0)
         perror(@"Error opening: ", err)
         repeat
@@ -62,6 +62,7 @@ PUB main() | err, dirent, ee_addr, ee_subpg
     ser.printf1(@"Opened %s\n\r", @_fname)
     ser.strln(@"dumping EEPROM to file...")
     repeat ee_addr from 0 to 65024 step 512
+        ser.pos_xy(0, 5)
         ser.printf1(@"EE addr: %d\n\r", ee_addr)
         { read 4 pages from the EE at a time to fill the SD buffer for better efficiency }
         repeat ee_subpg from 0 to 3

sdfat-fcreate.spin
@@ -35,7 +35,7 @@ OBJ
 DAT
 
     { filename to create must be 8.3 format; pad with spaces if less than full length }
-    _fname byte "TEST0015.TXT", 0
+    _fname byte "TEST0000.TXT", 0
 
 PUB main() | err, dirent
 